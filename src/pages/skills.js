12a0ace91f9a8efd6bc973e9ccad341b50294052
Maddie Higgins <maddiehiggins9@gmail.com>
2020-11-10 18:09:14 -0600

    intial population of skills page

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -4,7 +4,12 @@ import { Layout, SEO, HomeLink } from "../components"
 const Skills = () => (
   <Layout>
     <SEO title='skills' />
-      <div>Skills</div>
+      <h3>Skills</h3>
+      <div>Javascript / ES2020</div>
+      <div>React</div>
+      <div>Redux</div>
+      <div>PostgreSQL</div>
+      <div>Firestore</div>
     <HomeLink />
   </Layout>
 )
