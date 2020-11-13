8e760ec43301de458a2a3d56bab29b5988d94708
Maddie Higgins <maddiehiggins9@gmail.com>
2020-11-12 20:17:01 -0600

    add hamburger icon

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -1,15 +1,32 @@
-import React from "react"
+import React, { useState} from "react"
 import NavbarLink from "./navbar-link"
 import styles from "./navbar.module.css"
+import HamburgerMenu from "react-hamburger-menu"
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  }
+
   return (
     <div className={styles.navbar} >
-      <i class="fas fa-bars"></i>
-      <NavbarLink name='home' />
+      <HamburgerMenu
+        isOpen={open}
+        menuClicked={handleClick}
+        width={40}
+        height={20}
+        strokeWidth={5}
+        rotate={0}
+        color='black'
+        borderRadius={8}
+        animationDuration={0.5}
+      />
+      {/* <NavbarLink name='home' />
       <NavbarLink name='projects' />
       <NavbarLink name='skills' />
-      <NavbarLink name='references' />
+      <NavbarLink name='references' /> */}
     </div>
   )
 }
