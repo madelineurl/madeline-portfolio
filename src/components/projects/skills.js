265f35f987c55fc9374f15cbc80ba6477084d0f2
Maddie Higgins <maddiehiggins9@gmail.com>
2020-11-13 13:23:26 -0600

    update skills

--- src/components/projects/skills.js
+++ src/components/projects/skills.js
@@ -3,15 +3,24 @@ import styles from "./project.module.css"
 
 const Skills = () => (
   <div className={styles.skills}>
-    <div>Javascript / ES2020</div>
-    <div>CSS + SASS</div>
-    <div>React</div>
-    <div>Redux</div>
-    <div>Node.js</div>
-    <div>Express</div>
-    <div>SQL / Sequelize</div>
-    <div>PostgreSQL</div>
-    <div>Firestore</div>
+    <div>
+      <h3>frontend</h3>
+      <div>Javascript / ES2020</div>
+      <div>CSS3 + SASS</div>
+      <div>React</div>
+      <div>Redux</div>
+      <div>HTML5</div>
+      <div>Webpack</div>
+    </div>
+    <div>
+      <h3>backend</h3>
+      <div>Node.js</div>
+      <div>Express</div>
+      <div>SQL / Sequelize</div>
+      <div>GraphQL</div>
+      <div>PostgreSQL</div>
+      <div>Firestore</div>
+    </div>
   </div>
 )
 
