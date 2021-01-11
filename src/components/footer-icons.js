aa8a1bb5b6493e5d197a5d4a4f6021d5bfe91c5b
Maddie Higgins <maddiehiggins9@gmail.com>
2021-01-11 09:56:17 -0600

    making a mess

---
+++ src/components/footer-icons.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "gatsby";
+import Icon from "./icon";
+import contact from "./icons/mail.webp";
+import github from "./icons/github.webp";
+import linkedin from "./icons/linkedin.webp";
+import medium from "./icons/medium.webp";
+
+const FooterIcons = () => {
+  return (
+    <div className='flex'>
+       <Link to='/contact/' >
+          <img
+            src={contact}
+            alt="contact"
+            className='footer-icon'
+          />
+        </Link>
+        <Icon
+            url='https://github.com/madelineurl'
+            image={github}
+          />
+        <Icon
+        image={linkedin}
+        url='https://www.linkedin.com/in/madeline-url/'
+      />
+        <Icon
+        image={medium}
+        url='https://madeline-url.medium.com'
+      />
+    </div>
+  );
+};
+
+
+  export default FooterIcons;
