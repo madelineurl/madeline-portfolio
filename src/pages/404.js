571cafd89d6e91d58555c79cb343afc4a0178758
Maddie Higgins <maddiehiggins9@gmail.com>
2022-09-20 01:06:57 -0500

    finishing cleanup touches

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,12 @@
 import React from "react";
-import { Link } from "gatsby";
-import { Layout } from "../components";
-
-const style = {
-  display: `flex`,
-  flexDirection: 'column',
-  justifyContent: `center`,
-  fontFamily: `Bebas Neue`,
-  margin: '1rem 0'
-}
+import { Layout, BackToHomeLink } from "../components";
 
 const NotFoundPage = () => (
   <Layout pageTitle="404">
-    <h3 style={{...style, color: '#fff' }}>
+    <p id="not-found" className="flex">
      You've lost your way
-    </h3>
-    <Link to='home' className='link'>
-      <h1 id="lost" style={style} className="enlarge">Home</h1>
-    </Link>
+    </p>
+   <BackToHomeLink />
   </Layout>
 );
 
