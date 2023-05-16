840f41287effb67dc2d189c0a3fd8e4cf2bf45ba
Madeline Higgins <madelinehiggins@Madelines-MacBook-Pro.local>
2023-05-15 23:36:50 -0500

    more updates

--- src/components/landing-page-navigation.js
+++ src/components/landing-page-navigation.js
@@ -3,10 +3,13 @@ import React from 'react';
 const LandingPageNavigation = () => {
   return (
     <div>
-      <h1
-        id='madeline'>
-          madeline
-      </h1>
+      <div style={{ height: '100vh'}}>
+        <h1
+          id='madeline'>
+            madeline
+        </h1>
+      </div>
+      <div id="bio">MY BIO</div>
     </div>
   );
 };
