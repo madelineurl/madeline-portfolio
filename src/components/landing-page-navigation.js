abf29d5e4b02d115cbb15604d8949457ef2602fe
Madeline Higgins <madelinehiggins@Madelines-MacBook-Pro.local>
2023-05-16 00:11:37 -0500

    initial anchor linking

--- src/components/landing-page-navigation.js
+++ src/components/landing-page-navigation.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import caretDown from '../assets/icons/svg/caret_down.svg'
+import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 const LandingPageNavigation = () => {
   return (
-    <div>
-      <div style={{ height: '100vh'}}>
+    <div id="landing-image">
+      <AnchorLink to="/#bio">
+      <div>
         <h1
           id='madeline'>
             madeline
         </h1>
+        <img id="caret" src={caretDown} alt="down arrow to view the bio below the fold"/>
       </div>
-      <div id="bio">MY BIO</div>
+      </AnchorLink>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,10 @@ import { LandingPageNavigation } from '../components';
 const IndexPage = () => {
   return (
    <Layout pageTitle='home'>
-      <div id='landing'>
-        <div className='header-main'>
+        <div className='madeline-container'>
           <LandingPageNavigation />
         </div>
-      </div>
+        <div id="bio">Madeline may be one of @smartbarchicago's newest residents, but their vocal-forward club sets have been rousing Chicago dance floors for almost a decade. Sensual vocals and thundering toms imbue the @gramaphonerecords alum's sound with a characteristic warmth during peak-time sets at Smartbar's iconic weekly party Queen! and beyond. A seasoned radio host, disciplined student of music history, and skilled DJ in both vinyl and digital formats, Madeline's club-and techno-tinged house selections weave the genre-defining sounds of Chicago into the varied sonic fabric of the future.</div>
    </Layout>
   )
 };
